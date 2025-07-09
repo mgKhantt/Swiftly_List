97c174b7b8657070095c7ec3579ee9bec3b50b05
mgKhantt <khantphonenaing156@gmail.com>
2025-07-09 21:42:41 +0630

    Initial Commit

---
+++ to_do_list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to_do_list
+//
+//  Created by Khant Phone Naing  on 09/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ to_do_listTests/to_do_listTests.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listTests.swift
+//  to_do_listTests
+//
+//  Created by Khant Phone Naing  on 09/07/2025.
+//
+
+import Testing
+@testable import to_do_list
+
+struct to_do_listTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
